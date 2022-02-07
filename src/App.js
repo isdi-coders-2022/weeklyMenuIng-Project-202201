672b62ff76c06e19126a72c2d0c010ee64ffdaed
Albert Domínguez <albertdc1984@gmail.com>
2022-02-07 21:16:33 +0100

    fixed indentation issues

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         image="https://static2.hoy.es/www/pre2017/multimedia/noticias/200903/20/Media/supermario.jpg"
         title="MARIO"
         description="Super Mario is a platform game series created by Nintendo based on and starring the fictional plumber Mario. Alternatively called the Super Mario Bros.[b] series or simply the Mario[c] series, it is the central series of the greater Mario franchise. At least one Super Mario game has been released for
-         every major Nintendo video game console. There are over twenty games in the series."
+        every major Nintendo video game console. There are over twenty games in the series."
       />
     </div>
   );
