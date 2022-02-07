ee69df97f94b56d3e4bd583e41c39e9664eda670
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-07 16:49:52 +0100

    styled burger in unactive state

--- src/components/BurgerComponent/BurgerComponent.js
+++ src/components/BurgerComponent/BurgerComponent.js
@@ -1,10 +1,25 @@
+import styledComponents from "styled-components";
+
 const BurgerComponent = () => {
+  const BurgerContainer = styledComponents.div`
+  display: flex;
+  flex-direction: column;
+  height: 30px;
+  justify-content: space-between;
+  width: min-content;
+  `;
+  const BurgerBar = styledComponents.span`
+  width: 33px;
+  height: 3px;
+  background-color: #000;
+  
+  `;
   return (
-    <div className="burger">
-      <span className="burger-component"></span>
-      <span className="burger-component"></span>
-      <span className="burger-component"></span>
-    </div>
+    <BurgerContainer>
+      <BurgerBar />
+      <BurgerBar />
+      <BurgerBar />
+    </BurgerContainer>
   );
 };
 
