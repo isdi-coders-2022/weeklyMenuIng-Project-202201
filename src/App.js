dc44f31d8bbf9bae17ed709b494be16376679b84
Albert Domínguez <albertdc1984@gmail.com>
2022-02-07 20:04:06 +0100

    added stylews to checkbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Infoblock from "./components/Infoblock/Infoblock";
 import BurgerComponent from "./components/BurgerComponent/BurgerComponent";
-
+import Checkbox from "./components/Checkbox/Checkbox";
 function App() {
   return (
     <div className="App">
       <Infoblock text="My Weekly Menu" />
       <BurgerComponent />
+      <Checkbox className="julai" text="Friday" />
     </div>
   );
 }
