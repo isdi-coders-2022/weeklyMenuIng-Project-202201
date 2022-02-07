9955de525ea2bf0a82103f4d64f82d44625be0cd
Albert Domínguez <albertdc1984@gmail.com>
2022-02-07 15:44:29 +0100

    added App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+describe("Given an App react component", () => {
+  describe("When it's called", () => {
+    test("Then it should render a p element with the text Hola", () => {
+      const p = "Hola";
+    });
+  });
+});
