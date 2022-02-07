02041c1a612276cea8dac6ebadc66a76037aa841
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-07 21:59:18 +0100

    added react-router and added routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import Infoblock from "./components/Infoblock/Infoblock";
 import BurgerComponent from "./components/BurgerComponent/BurgerComponent";
 import Button from "./components/Button/Button";
+import NewRecipePage from "./pages/NewRecipePage";
+import EditRecipePage from "./pages/EditRecipePage";
+import MyMenuPage from "./pages/MyMenuPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import RecipePage from "./pages/RecipePage";
+import SearchRecipesPage from "./pages/SearchRecipesPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Infoblock text="My Weekly Menu" />
       <BurgerComponent />
       <Button
@@ -12,7 +20,22 @@ function App() {
         className="button"
         actionOnClick={() => window.alert("hola, soy un button!!")}
       />
-    </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home">
+          <Route index element={<MyMenuPage />} />
+          <Route path="view/:id:api" element={<RecipePage />} />
+          <Route path="new" element={<NewRecipePage />} />
+          <Route path="edit/:id" element={<EditRecipePage />} />
+        </Route>
+        <Route path="/search">
+          <Route index element={<SearchRecipesPage />} />
+          <Route path="view/:id:api" element={<RecipePage />} />
+        </Route>
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
 
