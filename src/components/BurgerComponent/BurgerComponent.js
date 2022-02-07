bb9315e5516f193c025e5a7795ad4211a1328167
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-07 16:35:32 +0100

    created html for burger component

---
+++ src/components/BurgerComponent/BurgerComponent.js
@@ -0,0 +1,11 @@
+const BurgerComponent = () => {
+  return (
+    <div className="burger">
+      <span className="burger-component"></span>
+      <span className="burger-component"></span>
+      <span className="burger-component"></span>
+    </div>
+  );
+};
+
+export default BurgerComponent;
