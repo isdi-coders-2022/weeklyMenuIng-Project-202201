4be671497c3697b005177cc848f2c733c237be2a
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-07 20:23:09 +0100

    added the styled components of header and links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Infoblock from "./components/Infoblock/Infoblock";
-import BurgerComponent from "./components/BurgerComponent/BurgerComponent";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Infoblock text="My Weekly Menu" />
-      <BurgerComponent />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import BurgerComponent from "../BurgerComponent/BurgerComponent";
+import Links from "../Links/Links";
+import logo from "./logo.svg";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  height: 100px;
+  align-items: center;
+  background-color: #b3dee2;
+  justify-content: space-between;
+
+  & ul {
+    display: flex;
+    width: 70%;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0;
+    list-style: none;
+    margin: 0;
+
+    & li a {
+      text-decoration: none;
+      color: #000;
+    }
+  }
+`;
+
+const Logo = styled.img`
+  height: inherit;
+`;
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <Logo src={logo} alt="My weekly menu logo" />
+      <Links />
+      <BurgerComponent />
+    </HeaderContainer>
+  );
+};
+export default Header;
