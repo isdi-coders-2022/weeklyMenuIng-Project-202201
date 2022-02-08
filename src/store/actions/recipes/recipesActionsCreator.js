11378e357ed71deb648c23dec3980136bee65f3c
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-08 14:07:02 +0100

    created actions creator for recipes

--- src/store/actions/recipes/recipesActionsCreator.js
+++ src/store/actions/recipes/recipesActionsCreator.js
@@ -1,1 +1,16 @@
-//recipesAcitonsCreator
+import { actionTypes } from "./recipesActionsTypes";
+
+export const createRecipeAciton = (recipe) => ({
+  type: actionTypes.createRecipe,
+  recipe,
+});
+
+export const removeRecipeAction = (id) => ({
+  type: actionTypes.removeRecipe,
+  id,
+});
+
+export const updateRecipe = (recipe) => ({
+  type: actionTypes.updateRecipe,
+  recipe,
+});
