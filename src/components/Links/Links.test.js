a5c3ca47f4154abb5c380e18b1abb6f751ada81c
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-08 10:53:53 +0100

    tested links component

---
+++ src/components/Links/Links.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Links from "./Links";
+
+describe("Given a Links component", () => {
+  describe("When it's rendered", () => {
+    test("then it shoul display the links text", () => {
+      const linksText = [
+        "My Weekly Menu",
+        "Search Recipes",
+        "Create New Recipe",
+        "About",
+      ];
+
+      render(
+        <BrowserRouter>
+          <Links />
+        </BrowserRouter>
+      );
+
+      linksText.forEach((textLink) => {
+        const text = screen.queryByText(textLink);
+        expect(text).toBeInTheDocument();
+      });
+    });
+  });
+});
