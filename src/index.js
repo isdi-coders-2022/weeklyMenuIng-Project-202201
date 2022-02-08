0e0ec066b395cc70147c5f0cc45d42d17052451f
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-08 19:28:33 +0100

    added custom hook useAPI and providers burguer

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import "./index.css";
 import App from "./App";
 import "@fontsource/montserrat";
 import { BrowserRouter } from "react-router-dom";
+import RecipesContextProvider from "./store/contexts/RecipesContext/RecipesContextProvider";
+import APIContextProvider from "./store/contexts/ApiContext/ApiContextProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <APIContextProvider>
+      <RecipesContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </RecipesContextProvider>
+    </APIContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
