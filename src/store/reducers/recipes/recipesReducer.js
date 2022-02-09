933a5fbd534b25704d996bbf4e78101f6ee94f1e
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-09 10:01:39 +0100

    implemented the recipes reducer for each action

--- src/store/reducers/recipes/recipesReducer.js
+++ src/store/reducers/recipes/recipesReducer.js
@@ -1,1 +1,30 @@
-//recipesReducer
+import { actionTypes } from "../../actions/recipes/recipesActionsTypes";
+
+const recipesReducer = (currentState, action) => {
+  let newState;
+
+  switch (action.type) {
+    case actionTypes.createRecipe:
+      newState = [...currentState, action.recipe];
+      break;
+    case actionTypes.loadRecipes:
+      newState = [...currentState, action.recipes];
+      break;
+    case actionTypes.removeRecipe:
+      newState = [...currentState].filter((recipe) => recipe.id !== action.id);
+      break;
+    case actionTypes.updateRecipe:
+      newState = [...currentState].map((recipe) =>
+        recipe.id === action.recipe.id ? action.recipe : recipe
+      );
+      break;
+
+    default:
+      newState = [...currentState];
+      break;
+  }
+
+  return newState;
+};
+
+export default recipesReducer;
