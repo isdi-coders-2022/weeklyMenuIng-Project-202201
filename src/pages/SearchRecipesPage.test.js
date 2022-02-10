b11951116235c1846fcf0df9c36f85083808a765
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-02-10 22:42:16 +0100

    added tests app

--- src/pages/SearchRecipesPage.test.js
+++ src/pages/SearchRecipesPage.test.js
@@ -1,21 +1,21 @@
 import { render, screen } from "@testing-library/react";
+
 import ApiContextProvider from "../store/contexts/ApiContext/ApiContextProvider";
 import MyRecipesContextProvider from "../store/contexts/RecipesContext/RecipesContextProvider";
-import RecipePage from "./NewRecipePage";
+import SearchRecipesPage from "./SearchRecipesPage";
 
-describe("Given a Recipe page", () => {
+describe("Given a SearchRecipesPage page", () => {
   describe("When it's rendered", () => {
-    test("Then it should render a title", () => {
+    test("Then it should render a SearchForm Component", () => {
       render(
         <ApiContextProvider>
           <MyRecipesContextProvider>
-            <RecipePage />
+            <SearchRecipesPage />
           </MyRecipesContextProvider>
         </ApiContextProvider>
       );
-      const recipe = screen.queryByRole("heading");
-
-      expect(recipe).toBeVisible();
+      const list = screen.queryByRole("list");
+      expect(list).not.toBeNull();
     });
   });
 });
