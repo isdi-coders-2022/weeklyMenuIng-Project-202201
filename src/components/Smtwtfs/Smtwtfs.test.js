06666c62bc3ffd060b5b422e18f79e85156cc703
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-02-10 19:21:29 +0100

    Passed first test for smtwtfs

--- src/components/Smtwtfs/Smtwtfs.test.js
+++ src/components/Smtwtfs/Smtwtfs.test.js
@@ -5,13 +5,21 @@ describe("Given a Smtetfs component", () => {
   const edamamJSON = require("./edamamResponse.json");
   const edamamReceipes = edamamJSON.hits;
   const edamamRecipe = edamamReceipes[3];
+  const days = [
+    { index: null, day: "sunday", active: false, initial: "S" },
+    { index: null, day: "monday", active: false, initial: "M" },
+    { index: null, day: "tuesday", active: false, initial: "T" },
+    { index: null, day: "wednesday", active: false, initial: "W" },
+    { index: null, day: "thursday", active: false, initial: "T" },
+    { index: null, day: "friday", active: false, initial: "F" },
+    { index: null, day: "saturday", active: false, initial: "S" },
+  ];
 
   describe("When instantiated with an edamam object as props", () => {
     test("Then it should render an element with the recipe label as title", () => {
-      render(<Smtwtfs smtwtfs={(0, 1, 2, 3, 4, 5, 6)} recipe={edamamRecipe} />);
+      render(<Smtwtfs smtwtfs={days} recipe={edamamRecipe} />);
 
-      const diaS = screen.queryByText("S");
-
+      const diaS = screen.queryByText("M");
       expect(diaS).toBeInTheDocument();
     });
   });
