0dc5b0eb1debd57469b66dd0331f380c82512989
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-10 10:21:07 +0100

    fixed backdrop-filter in browsers wich don't suport it

--- src/components/FloatingMenu/FloatingMenu.js
+++ src/components/FloatingMenu/FloatingMenu.js
@@ -10,10 +10,17 @@ const Menu = styled.div`
   width: 330px;
   margin: 0 auto;
   background: rgba(183, 56, 227, 0.55);
-  backdrop-filter: blur(5px);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
   z-index: 100;
+
+  @supports (-webkit-backdrop-filter: none) or (backdrop-filter: none) {
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
+  }
+  @supports not ((-webkit-backdrop-filter: none) or (backdrop-filter: none)) {
+    background: rgba(207, 123, 236, 0.99);
+  }
   & ul {
     padding: 0;
     margin: 0;
@@ -24,6 +31,7 @@ const Menu = styled.div`
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
+
     & li a {
       text-decoration: none;
       color: #000;
