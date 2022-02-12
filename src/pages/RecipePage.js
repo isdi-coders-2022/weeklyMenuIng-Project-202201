c37b4e27388c210c35351b6714c9efc05f58b4b3
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-02-12 19:00:51 +0100

    recipe component ready

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -1,18 +1,10 @@
 import Recipe from "./../components/Recipe/Recipe";
 
-const edamamJSON = require("./../edamamResponse.json");
-const edamamReceipes = edamamJSON.hits;
-const edamamRecipe = edamamReceipes[3];
-
 const RecipePage = () => {
   return (
     <>
       <main className="main">
         <h1>Recipe Page</h1>
-        <Recipe
-          className={"recipe-view"}
-          recipe={edamamRecipe}
-        />
       </main>
     </>
   );
