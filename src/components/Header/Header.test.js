833927bb64f4161187f9d96a9b4ebee2903044ad
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-13 17:01:52 +0100

    tested burger-header implementation

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 import Header from "./Header";
 import TestRenderer from "react-test-renderer";
 
@@ -26,6 +27,35 @@ describe("Given a Header Component", () => {
       ).toJSON();
 
       expect(header).toMatchSnapshot();
+    });
+  });
+
+  describe("Wher it's rendered and cilcked on the burger menu", () => {
+    test("Then the burger menu should have class active", () => {
+      render(
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      );
+
+      const burger = screen.queryByTitle("Navigation Menu");
+      userEvent.click(burger);
+
+      expect(burger).toHaveClass("active");
+    });
+  });
+  describe("Wher it's rendered and cilcked two times on the burger menu", () => {
+    test("Then the burger menu should not have class active", () => {
+      render(
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      );
+
+      const burger = screen.queryByTitle("Navigation Menu");
+      userEvent.dblClick(burger);
+
+      expect(burger).not.toHaveClass("active");
     });
   });
 });
